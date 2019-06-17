71c918917a0db464f20d249c62aa6a5371dbf32a
mike evans <evansmj@dukes.jmu.edu>
2019-06-17 10:50:49 -0400

    Add a customer and movie.

--- src/com/oldgoat5/Main.java
+++ src/com/oldgoat5/Main.java
@@ -3,6 +3,13 @@ package com.oldgoat5;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Customer customer = new Customer("ME");
+
+        Movie gradle = new Movie("Deep Dive Into Gradle", 001);
+        Rental gradleRental = new Rental(gradle, 3);
+
+        customer.addRental(gradleRental);
+
+        System.out.print("Customer '" + customer.getName() + "'\n\n" + customer.statement());
     }
 }
